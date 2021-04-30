1632acc7b07cac8d63dff5769f32f5e826b041b7
shalinisuryavanshi17 <65484483+shalinisuryavanshi17@users.noreply.github.com>
2021-04-30 22:02:33 +0530

    heading added

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
+      <div><h1 style={{textAlign:"center"}}>My Contacts</h1></div>
       <Contact name="Gajbinkar Shalini" image="http://thenewcode.com/assets/images/thumbnails/sarah-parmenter.jpeg"  tel="+ 123456789" email="shalini@gmail.com"/>
       <Contact name="Pandiyala Kavya" image="https://cdn.mcparfum.de/Avatare/9824063B-2CEF-4C1D-87A5-C33A2792B4FB.png"  tel="+ 1638527415" email="kavya@gmail.com"/>
       <Contact name="Divya Katta" image="https://cdn.mcparfum.de/Avatare/E6E892CC-E67A-4919-AB75-88CA61346CB8.png"  tel="+ 4561237895" email="divya@gmail.com"/>
